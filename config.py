aa1b5f7a21b2a5d7813c16346601122b5c8313b9
zhch <zhch@163.com>
2017-05-21 15:56:46 +0800

    config edit

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
 src_path='/home/zhch/nutstore/wordpress'
-# src_path='/home/zhch/temp/d3/nut_wordpress/'
 dest_path='/home/zhch/temp/static_blog'
 
-file_save_path="d:\\temp\\download_jd\\chenhr"  # 下载文件保存的位置
-start_url="http://petro.chenhr.com" # 开始链接
-#site_pre="http://petro.chenhr.com" # 网站的host
-detail_pre="http://www.chenhr.com" # detail 前缀
-proxyBean={"ip":"114.215.108.168", "port":36666}
+# for aliyun
+# src_path='/home/zhch/git_repo/nut_wordpress'
+# dest_path='/home/zhch/document/blog_static'
